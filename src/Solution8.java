620ff0c7f819cba214cca0c424d4b3b7b1362c0a
Faragon02 <chj20202@naver.com>
2025-03-07 08:57:37 +0900

    updtate 250307

---
+++ src/Solution8.java
@@ -0,0 +1,25 @@
+public class Solution8 {
+    public int solution(int angle) {
+        int answer = 0;
+        if(angle > 0 && angle < 90)
+        {
+            answer = 1;
+        } else if (angle == 90) {
+            answer = 2;
+        }
+        else if(angle >90 && angle <180)
+        {
+            answer = 3;
+        }
+        else if( angle == 180)
+        {
+            answer =4;
+        }
+        else
+        {
+            answer = 0;
+            System.out.println("삼각형이 아닙니다.");
+        }
+        return answer;
+    }
+}
