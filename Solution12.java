e10116077fde6753ef7df84951872114b93fcd33
Faragon02 <chj20202@naver.com>
2025-03-11 09:19:43 +0900

    Solve 12 Update 20250311

---
+++ Solution12.java
@@ -0,0 +1,11 @@
+public class Solution12 {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for(int i : arr)
+        {
+            answer += i;
+        }
+        answer = answer / arr.length;
+        return answer;
+    }
+}
