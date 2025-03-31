2c794a5fd6d105fea32ef258e208d1af5617a88a
Faragon02 <chj20202@naver.com>
2025-03-31 10:36:57 +0900

    Solve28 Update 20250331

--- src/lv1/Solution28.java
+++ src/lv1/Solution28.java
@@ -1,4 +1,12 @@
 package lv1;
 
-public class Solution28 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class    Solution28 {
+    public int solution(int[] numbers) {
+        int answer = -1;
+        return answer = 45 - Arrays.stream(numbers).sum();
+    }
 }
