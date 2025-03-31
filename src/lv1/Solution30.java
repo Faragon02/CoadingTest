dfa53ee9eac5720fd7abcd3997723aca25a2fe98
Faragon02 <chj20202@naver.com>
2025-03-31 10:39:15 +0900

    Solve31 UPdate250331

--- src/lv1/Solution30.java
+++ src/lv1/Solution30.java
@@ -1,4 +1,22 @@
 package lv1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Solution30 {
+    public String solution(String s) {
+        String answer = "";
+        String[] stringArr = s.split("");
+        if(s.length() % 2 ==0){
+            List<String> stringList = new ArrayList<>();
+            stringList.add(stringArr[stringArr.length /2 -1]);
+            stringList.add(stringArr[stringArr.length /2]);
+            answer =stringList.stream().map(String::new).collect(Collectors.joining());
+        }else {
+            answer = stringArr[stringArr.length /2 ];
+        }
+        return answer;
+    }
 }
