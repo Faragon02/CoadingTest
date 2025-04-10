89d6ce225831b2bf5c08ac2d667fca3cf9d80119
Faragon02 <chj20202@naver.com>
2025-04-10 09:43:27 +0900

    Solove34 Update 20250410

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
-
-import lv1.Solution30;
-import lv2.Solution31;
-import lv2.Solution32;
-
-import java.util.Arrays;
-
+import lv2.Solution34;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        Solution32 solTest = new Solution32();
-
-        System.out.println(solTest.solution(new int[]{1,2,3,4}, new int[]{-3,-1,0,2}));
+        Solution34 solTest = new Solution34();
+        System.out.println(solTest.solution("Zbcdefg"));
     }
 }
--- src/lv2/Solution34.java
+++ src/lv2/Solution34.java
@@ -1,4 +1,15 @@
 package lv2;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Solution34 {
+    public String solution(String s) {
+        String answer = "";
+        List<String> toAscII  = Arrays.stream(s.split("")).map(String::new).collect(Collectors.toList());
+        Collections.sort(toAscII, Collections.reverseOrder());
+        return answer = toAscII.stream().map(String::valueOf).collect(Collectors.joining());
+    }
 }
