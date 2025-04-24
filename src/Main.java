7a1f12030dd554a6669eeb8293292761ed099528
Fragon02 <chj20202@naver.com>
2025-04-24 09:30:44 +0900

    Solve37 Update250424

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import lv2.Solution36;
+import lv2.Solution37;
 import lv3.Solution49;
 
 import java.util.Arrays;
@@ -8,8 +9,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        Solution36 solTest = new Solution36();
-        System.out.println(solTest.solution("1234"));
+        Solution37 solTest = new Solution37();
+        int[][] arr1 = new int[][]{{1,2},{2,3}};
+        int[][] arr2 = new int[][]{{3,4},{4,5}};
+        System.out.println(solTest.solution(arr1, arr2));
         // System.out.println( new String(Arrays.toString(solTest.solution(new int[]{2,1,3,4,1}))));
 
     }
